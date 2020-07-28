469c28d533232c72ca750fbedc7f16f6cc2cf56d
Amanda <apete1@umbc.edu>
2020-07-28 20:02:53 +0000

    Moved data reading/wrangling to a new file

--- readData.R
+++ readData.R
@@ -1,23 +1,25 @@
-# This R script uses NCF to recommend movies to users based on implicit feedback
-# in the movie lense dataset.
-
-library(tidyverse)
-library(magrittr)
+# This script reads and wrangles the data: Movie Lense and Pinterest
 
 # Read data ---------------------------------------------------------------
 
 # notes: 
+# - Pinterest (pinterest-20) and Movie Lense (ml-1m) data
 # - user & item indexes are 0 based.
 # - ml-1m.train.rating contains user/item/rating triplets (plus a timestamp)
 # - ml-1m.test.negative contains a list of 100 negatives and one positive for each user
 # - ml-1m.test.rating contains user/item/rating triplets (plus a timestamp) for 
 #   each user's single rating in ml-1m.test.negative. 
 
-url <- "https://github.com/hexiangnan/neural_collaborative_filtering/raw/master/Data/ml-1m.train.rating"
+
+file_prefix <- "pinterest-20"
+#file_prefix <- "ml-1m"
+base_name <- str_c("https://github.com/hexiangnan/neural_collaborative_filtering/raw/master/Data/", file_prefix)
+
+url <- str_c(base_name, ".train.rating")
 train_rating <- read_tsv(pins::pin(url), 
                          col_names = c("user","item", "rating", "timestamp")) 
 
-url <-  "https://github.com/hexiangnan/neural_collaborative_filtering/raw/master/Data/ml-1m.test.negative"
+url <-  str_c(base_name, ".test.negative")
 test_data <- pins::pin(url) 
 #test_rating   <- read_tsv(test_data[str_detect(test_data, "rating")], 
 #                          col_names = c("user","item", "rating", "timestamp"))
@@ -29,17 +31,12 @@ test_negative <- read_tsv(test_data[str_detect(test_data, "negative")],
 #test_negative <- read_tsv(pins::pin(url), 
 #                          col_names = FALSE) 
 
+
 # Variable definitions ----------------------------------------------------
 
 num_users <- max(train_rating$user) + 1
 num_items <- max(train_rating$item) + 1
 neg_pos_ratio_train <- 4 # Ratio of negative training samples to positive to select for training
-
-
-# Define model --------------------------------------------------------------
-
-# Source the code for the NCF model:
-source("NCF.R")
 
 
 # Data wrangling ----------------------------------------------------------
@@ -65,7 +62,7 @@ train_negative <-
   mutate(subsamp = map2(data, n*neg_pos_ratio_train, ~slice_sample(.x, n=.y))) %>% 
   select(user, subsamp) %>%
   unnest(cols = c(subsamp))
-  
+
 # Define validation data by picking the most recent rating for each user from training
 validation <- train_rating %>% 
   group_by(user) %>% 
@@ -79,50 +76,3 @@ train <- anti_join(train_rating, validation) %>%
   select(user, item) %>%
   bind_rows("pos" = ., "neg" = train_negative, .id = "label") %>%
   mutate(label = as.integer(str_detect(label,"pos")))
-
-
-# Train model -------------------------------------------------------------
-
-history <- 
-  model %>% 
-  fit(
-    x = list(user_input = as.array(train$user), 
-         item_input = as.array(train$item)),
-    y = as.array(train$label),
-    epochs = 10,
-    batch_size = 1024, #paper used 128
-    validation_data = list(list(user_input = as.array(validation$user), 
-                                item_input = as.array(validation$item)), 
-                           as.array(validation$label)),
-    shuffle = TRUE, #TODO: See if this will work.
-    callbacks = list(callback_early_stopping(patience = 2))
-  ) 
-
-
-# Evaluate results --------------------------------------------------------
-
-history
-#plot(history)
-
-# Evaluate returns same metrics that were defined in the compile (accuracy in this case)
-(results <- model %>% evaluate(list(test$user, test$item), test$label))
-
-# Get predictions for test set:
-test_pred <- model %>% 
-  predict(x = list(test$user, test$item)) %>%
-  bind_cols(pred = ., test)
-
-# Compute hit rate and ndcg
-source("evaluation.R")
-compute_hr(test_pred, 10)
-compute_ndcg(test_pred, 10)
-
-
-
-
-
-
-
-
-
-
---
+++ run.R
@@ -0,0 +1,64 @@
+# This R script uses NCF to recommend movies to users based on implicit feedback
+# in the movie lense dataset.
+
+# Read and wrangle data: ----------------------------------------------------
+
+source("readData.R")
+
+# Define model --------------------------------------------------------------
+
+source("NCF.R")
+
+# Train model -------------------------------------------------------------
+
+# First define callbacks to stop model early when validation loss increases and to save best model
+callback_list <- list(
+  callback_early_stopping(patience = 2),
+  callback_model_checkpoint(filepath = "model.h5", monitor = "val_loss", save_best_only = TRUE)
+)
+  
+history <- 
+  model %>% 
+  fit(
+    x = list(user_input = as.array(train$user), 
+         item_input = as.array(train$item)),
+    y = as.array(train$label),
+    epochs = 10,
+    batch_size = 2048, 
+    validation_data = list(list(user_input = as.array(validation$user), 
+                                item_input = as.array(validation$item)), 
+                           as.array(validation$label)),
+    shuffle = TRUE, 
+    callbacks = callback_list
+  ) 
+
+# Load best model:
+model <- load_model_hdf5("my_model.h5")
+
+# Evaluate results --------------------------------------------------------
+
+history
+#plot(history)
+
+# Evaluate returns same metrics that were defined in the compile (accuracy in this case)
+(results <- model %>% evaluate(list(test$user, test$item), test$label))
+
+# Get predictions for test set:
+test_pred <- model %>% 
+  predict(x = list(test$user, test$item)) %>%
+  bind_cols(pred = ., test)
+
+# Compute hit rate and ndcg
+source("evaluation.R")
+compute_hr(test_pred, 10)
+compute_ndcg(test_pred, 10)
+
+
+
+
+
+
+
+
+
+
