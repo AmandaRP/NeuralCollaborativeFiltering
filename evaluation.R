c849fcb2b5953c0cc7c05d1da83b8b872e3b42ed
Amanda <apete1@umbc.edu>
2020-07-28 16:25:42 +0000

    Added ndcg evaluation metric

--- evaluation.R
+++ evaluation.R
@@ -12,6 +12,14 @@ compute_hr <- function(test_pred, k){
 
 
 # Compute ndcg:
+# Note that the current implementation assumes one 1 for each user. If this is not the case, need to divide by an idcg.
 compute_ndcg <- function(test_pred, k){
-  
+  test_pred %>% 
+    group_by(user) %>% 
+    slice_max(order_by = pred, n = k) %>%
+    mutate(rank = rank(desc(pred), ties.method = "random")) %>% 
+    mutate(dcg = (2^label-1)/log(rank+1, base = 2)) %>%
+    summarize(ndcg_user = sum(dcg)) %>%
+    ungroup() %>%
+    summarize(ndcg = mean(ndcg_user))
 }
