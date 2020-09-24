051bf0d0e15abad3bdb939b6a905e68732aba50b
AmandaRP <amanda@dekleineorchards.com>
2020-09-24 01:20:52 +0000

    Got the python implicit neg sampling script to a good point. Tested on fake data.

--- sample_implicit_negatives.py
+++ sample_implicit_negatives.py
@@ -1,27 +1,25 @@
 import numpy as np
 import pandas as pd
 
-
-
-def samp_impl_neg( N, p, excludes=[] ):
-    item_indices = np.arange(0,int(N))
-    return np.random.choice( np.setdiff1d(item_indices, excludes), size=int(p) )
+def samp_impl_neg(item_ids , n, excludes=[] , p = None):
+    return np.random.choice( np.setdiff1d(item_ids, excludes), size=int(n) )
 
 # Input: 
-#   userIDs: array containing unique user IDs
-#   itemIDs: array containing unique item IDs
-#   num_ratings_2sample: vector specifying number of implicit negatives to samples for each user. ith entry should correspond to ith entry in UserIDs vector.
-#   df_exclude: dataframe containing user-item pairs that are already assigned (to datasets such as training, validation, and/or test)
-# TODO: Make df_exclude an optional argument
+#   userIDs: vector containing unique user IDs
+#   itemIDs: vector containing unique item IDs
+#   num_ratings_2sample: vector specifying number of implicit negatives to samples for each user. ith entry should correspond to ith entry in UserIDs vector.#   
+#   df_exclude: dataframe containing user-item pairs that are already assigned (to datasets such as training, validation, and/or test). Must include "user" and "item" columns
 # TODO: Add an optional probability vector for each item (could be used to assign higher probability to popular items)
-def sample_implicit_negatives(userIDs, itemIDs, num_ratings_2sample, df_exclude):
-
-  # group the pandas df by user
-  df_gpd = train.groupby("user")
-
-  # iterate to create positive lists
-  df_users = [ (user,np.array(pd.DataFrame(df_small).item, dtype=int)) for (user,df_small) in df_gpd]
-
-  sampled_negative = [ (u, samp_impl_neg(r.num_items, len(its)*r.neg_pos_ratio_train, excludes=its)) for (u,its) in df_users ]
+def sample_implicit_negatives(user_ids, num_items_to_sample, item_ids, df_exclude, p = None):
+    # dictionary for user ids and associated number of items to sample
+    d = {k:v for (k,v) in zip(user_ids, num_items_to_sample)}
+    # group the pandas df by user
+    gpd = df_exclude.groupby("user")
+    # iterate to create exclude lists
+    user_exclude_items = [ (user, np.array(pd.DataFrame(df_small).item, dtype=int)) for (user, df_small) in gpd]
+    implicits = [ (user, samp_impl_neg(item_ids, d[user], excludes=items)) for (user, items) in user_exclude_items]
+    df = pd.DataFrame(implicits, columns = ["user", "item"])
+    return df.explode("item", ignore_index = True)
   
-  return sampled_negatives
+  
+
